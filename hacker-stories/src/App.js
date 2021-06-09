ba373d9d3dd36bb43bd11acd00be935384ea8a25
Kevin Hudgens <hudgens1073@gmail.com>
2021-06-09 18:22:28 +0900

    clean up

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -1,51 +1,16 @@
 import './App.css';
 
-function Header(props){
-  console.log(props);
-  return (
-    <header>
-      <h1>{props.name}'s Kitchen</h1>
-    </header>
-  )
+function getTitle(title) {
+  return title;
 }
-
-function Main(props){
-  console.log(props);
-  return(
-    <section>
-    <p>We serve the most {props.adjective} food around.</p>
-    <ul style={{ textAlign: "left"}}>
-      {props.dishes.smap((dish, i)=> (
-      <li key={i}>{dish}</li>)
-        )
-      }
-    </ul>
-    </section>
-  )
-}
-
-function Footer(props){
-  return(
-    <footer>
-      Copyright {props.year}
-    </footer>
-  )
-}
-
-const dishes = [
-  "Macaroni and Cheese",
-  "Salmon",
-  "Sushi"
-]
-
-dishes.map((dish) => console.log(dish))
 
 function App() {
   return (
-    <div className="App">
-      <Header name="Appa"/>
-      <Main adjective="amazing" dishes={dishes}/>
-      <Footer year={new Date().getFullYear()}/>
+    <div>
+      <h1>Hello {getTitle('React')}</h1>
+
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
     </div>
   );
 }
