1566318d08e6d401d45f05a2a0619f0b04a26592
Kevin Hudgens <hudgens1073@gmail.com>
2021-06-09 16:51:08 +0900

    added properties

--- react-essentials/src/App.js
+++ react-essentials/src/App.js
@@ -1,25 +1,27 @@
 import './App.css';
 
-function Header(){
+function Header(props){
+  console.log(props);
   return (
     <header>
-      <h1>Kevin's Kitchen</h1>
+      <h1>{props.name}'s Kitchen</h1>
     </header>
   )
 }
 
-function Main(){
+function Main(props){
+  console.log(props);
   return(
     <section>
-    <p>We serve the most delicious food around.</p>
+    <p>We serve the most {props.adjective} food around.</p>
     </section>
   )
 }
 
-function Footer(){
+function Footer(props){
   return(
     <footer>
-      Its true.
+      Copyright {props.year}
     </footer>
   )
 }
@@ -27,9 +29,9 @@ function Footer(){
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Main />
-      <Footer />
+      <Header name="Appa"/>
+      <Main adjective="amazing"/>
+      <Footer year={new Date().getFullYear()}/>
     </div>
   );
 }
